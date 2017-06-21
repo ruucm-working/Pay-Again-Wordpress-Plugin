a02ef2aa225afd53141800da0e20f4bc5dfbe000
YONG JI <bumbermin@gmail.com>
2017-06-21 20:36:59 +0900

    (🐜) rename jquery enque script function name my_jquery_enqueue to pay_again_jquery_enqueue to resolve conflict name error with jaranda-ui-helper.php

--- pay-again-gateway.php
+++ pay-again-gateway.php
@@ -34,8 +34,8 @@ function woocommerce_add_pay_again_gateway($methods) {
 /**
  *	Add Jquery
  */
-if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
-function my_jquery_enqueue() {
+if (!is_admin()) add_action("wp_enqueue_scripts", "pay_again_jquery_enqueue", 11);
+function pay_again_jquery_enqueue() {
 	wp_deregister_script('jquery');
 	wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js", false, null);
 	wp_enqueue_script('jquery');
